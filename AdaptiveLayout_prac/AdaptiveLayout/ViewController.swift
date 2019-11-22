6d8f595bc2a48a0961b47938991659fca6803b7c
wiz_Dan <dan@wiz-Danui-iMac.local>
2019-11-22 20:04:59 +0900

    first commit

---
+++ AdaptiveLayout_prac/AdaptiveLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AdaptiveLayout
+//
+//  Created by wiz_Dan on 22/11/2019.
+//  Copyright © 2019 wiz_Dan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
